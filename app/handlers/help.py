9ea79723a085634f2382e060600ac983e0b042cf
XetPy1030 <xetpy1030@mail.ru>
2025-02-12 15:27:04 +0300

    Добавлена поддержка примеров команды

--- app/handlers/help.py
+++ app/handlers/help.py
@@ -1,9 +1,13 @@
 from app.handlers.routers import routers
 
 
-COMMAND_TEXT_FORMAT = "!{command}: {description}"
+COMMAND_TEXT_FORMAT = "{command}: {description}\n```{command_with_example_args}```"
 
 
 async def help_handler(event):
-    help_text = "\n".join([COMMAND_TEXT_FORMAT.format(command=router.command, description=router.description) for router in routers])
+    help_text = "\n".join([COMMAND_TEXT_FORMAT.format(
+        command=router.command,
+        description=router.description,
+        command_with_example_args=f"!{router.command} {router.example_args}".strip() if router.example_args is not None else ""
+    ).strip() for router in routers])
     await event.respond(help_text)
--- app/handlers/routers.py
+++ app/handlers/routers.py
@@ -8,19 +8,20 @@ class Router:
     command: str
     handler: callable
     description: str | None = None
+    example_args: str | None = None
 
 
 routers = [
     Router('ping', handlers.common.ping_handler, 'Ping command'),
     Router('id', handlers.common.id_handler, 'Get chat id'),
     Router('delete_all_goals', handlers.goals.delete_all_goal_handler, 'Delete all goals'),
-    Router('new_goal', handlers.goals.new_goal_handler, 'Create new goal'),
+    Router('new_goal', handlers.goals.new_goal_handler, 'Create new goal', '2025-02-14T03:00:00+03:00 New Goal!'),
     Router('goals', handlers.goals.goals_handler, 'List all goals'),
-    Router('force_new_goal', handlers.goals.force_new_goal_handler, 'Force create new goal'),
-    Router('repeat', handlers.repeats.repeat_handler, 'Create repeat message'),
-    Router('disable_repeat', handlers.repeats.disable_repeat_handler, 'Disable repeat message'),
+    Router('force_new_goal', handlers.goals.force_new_goal_handler, 'Force create new goal', 'Goal 828282 1 2025-02-14T03:00:00+03:00'),
+    Router('repeat', handlers.repeats.repeat_handler, 'Create repeat message', '1h Repeat'),
+    Router('disable_repeat', handlers.repeats.disable_repeat_handler, 'Disable repeat message', '1'),
     Router('repeats', handlers.repeats.repeats_handler, 'List all repeat messages'),
-    Router('new_schedule', handlers.schedules.new_schedule_handler, 'Create schedule message'),
-    Router('disable_schedule', handlers.schedules.disable_schedule_handler, 'Disable schedule message'),
+    Router('new_schedule', handlers.schedules.new_schedule_handler, 'Create schedule message', '2025-02-14T03:00:00+03:00 Schedule'),
+    Router('disable_schedule', handlers.schedules.disable_schedule_handler, 'Disable schedule message', '1'),
     Router('schedules', handlers.schedules.schedules_handler, 'List all schedule messages'),
 ]
