221fae81c455c07003c8171ac4a3cf332d772bf3
XetPy1030 <xetpy1030@mail.ru>
2025-02-12 15:33:04 +0300

    Фикс показа описания

--- app/handlers/help.py
+++ app/handlers/help.py
@@ -1,13 +1,19 @@
-from app.handlers.routers import routers
+from app.handlers.routers import routers, Router
 
-
-COMMAND_TEXT_FORMAT = "{command}: {description}\n```{command_with_example_args}```"
+COMMAND_TEXT_FORMAT = "{command}: {description}"
 
 
 async def help_handler(event):
     help_text = "\n".join([COMMAND_TEXT_FORMAT.format(
         command=router.command,
         description=router.description,
-        command_with_example_args=f"!{router.command} {router.example_args}".strip() if router.example_args is not None else ""
     ).strip() for router in routers])
     await event.respond(help_text)
+
+
+def get_description(router: Router) -> str:
+    description = router.description
+    if router.example_args is not None:
+        command_with_example_args = f"!{router.command} {router.example_args}".strip()
+        description = f"{description}\n```{command_with_example_args}```"
+    return description
