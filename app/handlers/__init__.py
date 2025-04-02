37f801b068388cf15cffc5afd554b0051b2e8de8
XetPy1030 <xetpy1030@mail.ru>
2025-04-02 19:38:07 +0400

    Фикс схожих команд

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -8,8 +8,8 @@ from app.telegram_client import client
 
 def setup_routers():
     for router in routers.routers:
-        client.on(events.NewMessage(pattern=f'!{router.command}', outgoing=True))(router.handler)
+        client.on(events.NewMessage(pattern=f'!{router.command}($| )', outgoing=True))(router.handler)
 
-    client.on(events.NewMessage(pattern='!help', outgoing=True))(help_handler)
+    client.on(events.NewMessage(pattern='!help($| )', outgoing=True))(help_handler)
     
     client.on(events.NewMessage(outgoing=False))(reaction_handler)
